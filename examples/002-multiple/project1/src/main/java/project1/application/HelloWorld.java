ab05ec12dec3b4581018e33f1c55e5908bb7473c
Karthik Vijayan <kvijayan@salesforce.com>
2019-09-25 16:05:39 -0500

    Add another sample with multiple projects

---
+++ examples/002-multiple/project1/src/main/java/project1/application/HelloWorld.java
@@ -0,0 +1,11 @@
+package project1.application;
+
+import mybuilder_sources.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        new MybuilderSources();
+        System.out.println("Hello World");
+    }
+}
